bbb4b183553f471cfa98c7d06062decdc855f7c3
David <davidponceg207@gmail.com>
2026-02-04 12:33:33 -0600

    Login Screen

--- src/presentation/screens/auth/LoginScreen.tsx
+++ src/presentation/screens/auth/LoginScreen.tsx
@@ -1,9 +1,58 @@
-import { Text, View } from 'react-native';
+import { Button, Input, Layout, Text } from '@ui-kitten/components';
+import { ScrollView, useWindowDimensions } from 'react-native';
 
 export const LoginScreen = () => {
+
+    const { height } = useWindowDimensions();
+
     return (
-        <View>
-            <Text>LoginScreen</Text>
-        </View>
+        <Layout style={{ flex: 1 }}>
+            <ScrollView style={{ marginHorizontal: 40 }}>
+
+                <Layout style={{ paddingTop: height * 0.35 }}>
+                    <Text category='h1'>Login</Text>
+                    <Text category='p2'>Please, login to continue</Text>
+                </Layout>
+
+                {/* Inputs */}
+                <Layout style={{ marginTop: 20 }}>
+                    <Input
+                        placeholder='email'
+                        keyboardType='email-address'
+                        autoCapitalize='none'
+                        style={{ marginBottom: 10 }}
+                    />
+
+                    <Input
+                        placeholder='password'
+                        autoCapitalize='none'
+                        secureTextEntry
+                        style={{ marginBottom: 10 }}
+                    />
+                </Layout>
+
+                {/* Space */}
+                <Layout style={{height: 20}} />
+
+                <Layout>
+                    <Button onPress={() => {}}>
+                        Login
+                    </Button>
+                </Layout>
+
+                {/* Info */}
+                <Layout style={{height: 50}} />
+
+                <Layout style={{
+                    alignItems: 'center',
+                    flexDirection: 'row',
+                    justifyContent: 'center'
+                }}>
+                    <Text>Don't you have an account?</Text>
+                    <Text status='primary' category='s1' onPress={() => {}}>{' '}New account{' '}</Text>
+                </Layout>
+
+            </ScrollView>
+        </Layout>
     )
 }
