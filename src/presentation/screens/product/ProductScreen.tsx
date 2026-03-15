0b257933f4e73854d9781d2c7dec77b2271fc6a8
David <davidponceg207@gmail.com>
2026-03-15 17:22:26 -0600

    Product Screen [Design]

--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -1,10 +1,12 @@
-import { Text } from "@ui-kitten/components"
+import { Input, Layout } from "@ui-kitten/components"
 import { MainLayout } from "../../layouts/MainLayout"
 import { useQuery } from "@tanstack/react-query"
 import { StackScreenProps } from "@react-navigation/stack"
 import { RootStackParams } from "../../navigation/StackNavigator"
 import { getProductById } from "../../../actions/products/get-product-by-id"
 import { useRef } from "react"
+import { FlatList, ScrollView } from "react-native"
+import { FadeInImage } from "../../components/ui/FadeInImage"
 
 interface Props extends StackScreenProps<RootStackParams, 'ProductScreen'>{}
 
@@ -22,7 +24,67 @@ export const ProductScreen = ({route}: Props) => {
     }
     return (
         <MainLayout title={ product.title } subTitle={`Price: ${product.price}`}>
-            <Text>Hello World</Text>
+            <ScrollView style={{flex: 1}}>
+
+                {/* Product Images */}
+                <Layout>
+                    <FlatList
+                        data={ product.images }
+                        keyExtractor={(item) => item}
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        renderItem={({item}) => (
+                            <FadeInImage uri={item} style={{
+                                width: 300, height: 300, marginHorizontal: 7
+                            }} />
+                        )}
+                    />
+                </Layout>
+
+                {/* Form */}
+                <Layout style={{ marginHorizontal: 10 }}>
+                    <Input
+                        label='Title'
+                        value={ product.title }
+                        style={{marginVertical: 5}}
+                    />
+                    <Input
+                        label='Slug'
+                        value={ product.slug }
+                        style={{marginVertical: 5}}
+                    />
+                    <Input
+                        label='Description'
+                        value={ product.description }
+                        multiline
+                        numberOfLines={5}
+                        style={{marginVertical: 5}}
+                    />
+                </Layout>
+
+                {/*  */}
+                <Layout style={{
+                    marginHorizontal: 15, flexDirection: 'row', gap: 10, marginVertical: 5
+                }}>
+
+                    <Input
+                        label='Precio'
+                        value={ product.price.toString() }
+                        style={{flex: 1}}
+                    />
+                    <Input
+                        label='Stock'
+                        value={ product.stock.toString() }
+                        style={{flex: 1}}
+                    />
+
+                </Layout>
+
+                {/*  */}
+
+                <Layout style={{ height: 200 }} />
+
+            </ScrollView>
         </MainLayout>
     )
 }
