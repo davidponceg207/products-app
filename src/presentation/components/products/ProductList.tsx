1419f6a04262f0eb8f2690d43aa2bd2ea3500aed
David <davidponceg207@gmail.com>
2026-03-16 19:51:01 -0600

    Update cache [Pull to refresh]

--- src/presentation/components/products/ProductList.tsx
+++ src/presentation/components/products/ProductList.tsx
@@ -3,6 +3,7 @@ import { Layout, List } from '@ui-kitten/components';
 import { ProductCard } from './ProductCard';
 import { useState } from 'react';
 import { RefreshControl } from 'react-native';
+import { useQueryClient } from '@tanstack/react-query';
 
 interface Props {
     products: Product[];
@@ -11,13 +12,14 @@ interface Props {
 
 export const ProductList = ({ products, fetchNextPage }: Props) => {
 
+    const queryClient = useQueryClient();
     const [isRefreshing, setIsRefreshing] = useState(false);
 
     const onPullToRefresh = async() => {
         setIsRefreshing(true);
 
-        await new Promise<void>(resolve => setTimeout(resolve, 1500))
-
+        await new Promise<void>(resolve => setTimeout(resolve, 200))
+        queryClient.invalidateQueries({queryKey: ['products', 'infinite']});
         setIsRefreshing(false);
     };
 
