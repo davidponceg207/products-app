039bba6aa2b1adf250a1685a19e33723283b7697
David <davidponceg207@gmail.com>
2026-03-21 16:16:16 -0600

    Camera adapter

--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -11,6 +11,7 @@ import { MyIcon } from "../../components/ui/MyIcon"
 import { Formik } from "formik"
 import { updateCreateProduct } from "../../../actions/products/update-create-product"
 import { ProductImages } from "../../components/products/ProductImages"
+import { CameraAdapter } from "../../../config/adapters/camera-adapter"
 
 const sizes: Size[] = [Size.Xs, Size.S, Size.M, Size.L, Size.Xl, Size.Xxl];
 const genders: Gender[] = [Gender.Kid, Gender.Men, Gender.Women, Gender.Unisex];
@@ -52,7 +53,10 @@ export const ProductScreen = ({route}: Props) => {
                     <MainLayout
                         title={ values.title }
                         subTitle={`Price: ${values.price}`}
-                        rightAction={() => console.log('Hello World')}
+                        rightAction={async() => {
+                            const photos = await CameraAdapter.takePicture();
+                            setFieldValue('images', [...values.images, ...photos])
+                        }}
                         rightActionIcon="camera-outline"
                     >
                         <ScrollView style={{flex: 1}}>
